866682adbb7c7471c04c5485de329efd10359289
root <root@ubuntu.(none)>
2013-10-09 11:26:54 +0300

    On ubuntu os expect package is needed

--- scppass.sh
+++ scppass.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+MY_DIR=$(dirname $(readlink -f $0))
+source $MY_DIR/get_os_version.sh
+
+GetOSVersion
+
+if [[ $os_PACKAGE -eq "deb" ]]; then
+        sudo apt-get install expect 1>/dev/null
+fi
 
 if [ $# -ne 3 ]; then
     echo "Usage: $0 <src> <dest> <ssh_password>"
@@ -9,6 +18,7 @@ fi
 SRC=$1
 DEST=$2
 PWD=$3
+
 
 /usr/bin/expect <<EOD
 spawn scp -oStrictHostKeyChecking=no -oCheckHostIP=no "$SRC" "$DEST"
